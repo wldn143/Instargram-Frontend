658f4978fadc0a6974ef4296ac2ff4faceac2b32
wldn143 <wldn143@gmail.com>
2023-01-12 23:45:52 +0900

    Feed Photo, ToggleLike

---
+++ src/components/Feed/Photo.js
@@ -0,0 +1,117 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FatText } from "../shared";
+import Avatar from "../auth/Avatar";
+import { gql, useMutation } from "@apollo/client";
+
+const TOGGLE_LIKE_MUTATION = gql`
+  mutation toggleLike($id: Int!) {
+    toggleLike(id: $id) {
+      ok
+      error
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+  max-width: 615px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+function Photo({ id, user, file, isLiked, likes }) {
+  const [toggleLike, { loading }] = useMutation(TOGGLE_LIKE_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Avatar lg url={user.avatar} />
+        <Username>{user.username}</Username>
+      </PhotoHeader>
+      <PhotoFile src={file} />
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction onClick={toggleLike}>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
+}
+
+Photo.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+};
+
+export default Photo;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,39 @@
+import { gql, useQuery } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 import { logUserOut } from "../apollo";
+import Photo from "../components/Feed/Photo";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments {
+        payload
+      }
+      createdAt
+      isMine
+      isLiked
+    }
+  }
+`;
 
 function Home() {
   const history = useHistory();
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
   return (
     <div>
-      <h1>Home</h1>
       <button onClick={() => logUserOut(history)}>로그아웃</button>
+      {data?.seeFeed?.map((photo) => (
+        <Photo key={photo.id} {...photo} />
+      ))}
     </div>
   );
 }
