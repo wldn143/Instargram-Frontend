8cfcdd5d50eba65257ef6e14b280b9d634fc6235
wldn143 <wldn143@gmail.com>
2023-01-14 18:33:10 +0900

    comment의 hashtags 파싱

--- src/components/Feed/Comment.js
+++ src/components/Feed/Comment.js
@@ -1,16 +1,36 @@
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
 
 const CommentContatiner = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 function Comment({ author, payload }) {
   return (
     <CommentContatiner>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/g.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContatiner>
   );
 }
